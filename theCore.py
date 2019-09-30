57a0755e69240c520c3a4c7f19de86ec7b7f63a1
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2019-09-30 09:16:47 +0200

    CodeSignal problems solution

--- theCore.py
+++ theCore.py
@@ -4,11 +4,39 @@ from collections import Counter
 
 
 class IntroGates:
+    #8 problems
     def __init__(self):
+        self.mintTwoDigits=self.addTwoDigits(n=15)
 
-    def addTwoDigits(self,n):
+
+    def addTwoDigits(self,n:int)->int:
+        a=n%10
+        b=int(n//10)
+        return a+b
 
 
+    def largestNumber(self)->int:
+        pass
+
+    def candies(self)->int:
+        pass
+
+    def seatsInTheater(self)->int:
+        pass
+
+
+    def maxMultiple(self)->int:
+        pass
+
+    def circleOfNumbers(self)->int:
+        pass
+
+
+    def lateRide(self)->int:
+        pass
+
+    def phoneCall(self)->int:
+        pass
 
 
 
@@ -20,28 +48,44 @@ class atTheCrossroads:
 
 class CornerOf0sAnd1s:
     def __init__(self):
-    pass
+        pass
 
 class LoopTunnel:
     def __init__(self):
-    pass
+        pass
 
 class ListForestEdge:
     def __init__(self):
-    pass
+        pass
 
 class LabyrinthOfNestedLoops:
     def __init__(self):
-    pass
+        pass
 
 class BookMarket:
     def __init__(self):
-    pass
+        pass
 
 class MirrorLake:
     def __init__(self):
-    pass
+        pass
 
 class WellOfIntegration:
     def __init__(self):
-    pass
+        pass
+
+
+if __name__ == "__main__":
+    introGates=IntroGates()
+    crossRoads=atTheCrossroads()
+    corner0sAnd1s=CornerOf0sAnd1s()
+    tunel=LoopTunnel()
+    forestedge=ListForestEdge()
+    nestedLoops=LabyrinthOfNestedLoops()
+    bookMarket=BookMarket()
+    mirrorLake=MirrorLake()
+    integration=WellOfIntegration()
+
+    print('the end')
+
+

7375fb128cc6805014a8685c5dbba5e5afd7bc7c
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2019-10-13 19:11:21 +0200

    CodeSignal problems solution

--- theCore.py
+++ theCore.py
@@ -39,8 +39,8 @@ class atTheCrossroads:
     def __init__(self):
         self.mbRpgNextLevel = self.reachNextLevel(experience=10, threshold=15, reward=5)
         self.mintknapsackLight = self.knapsackLight(value1=15, weight1=2, value2=20, weight2=3, maxW=2)
-        #self.mbIsInfinite=self.isInfiniteProcess(a=2,b=3)#does not work
-        self.btenis=self.tenisSet(score1=7,score2=7)
+        # self.mbIsInfinite=self.isInfiniteProcess(a=2,b=3)#does not work
+        self.btenis = self.tenisSet(score1=7, score2=7)
 
     def reachNextLevel(self, experience, threshold, reward):
         if experience + reward >= threshold:
@@ -51,45 +51,44 @@ class atTheCrossroads:
     def knapsackLight(self, value1: int, weight1: int, value2: int, weight2: int, maxW: int) -> int:
         if weight1 + weight2 <= maxW:
             return value1 + value2
-        elif min(weight2,weight1)>maxW:
+        elif min(weight2, weight1) > maxW:
             return 0
-        elif weight1<=maxW and weight2>maxW:
+        elif weight1 <= maxW and weight2 > maxW:
             return value1
-        elif weight2<=maxW and weight1>maxW:
+        elif weight2 <= maxW and weight1 > maxW:
             return value2
-        elif weight1<=maxW and weight2<=maxW and weight1+weight2>maxW:
-            return max(value1,value2)
+        elif weight1 <= maxW and weight2 <= maxW and weight1 + weight2 > maxW:
+            return max(value1, value2)
 
-
-    def extraNumber(self,a:int,b:int,c:int)->int:
-        if (a==b):
+    def extraNumber(self, a: int, b: int, c: int) -> int:
+        if (a == b):
             return c
         if (a == c):
             return b
         if (b == c):
             return a
 
-    def isInfiniteProcess(self,a:int,b:int)->bool:#does not work
-        while a!=b:
-            a+=1
-            b-=1
-        if a==b:
+    def isInfiniteProcess(self, a: int, b: int) -> bool:  # does not work
+        while a != b:
+            a += 1
+            b -= 1
+        if a == b:
             return False
         else:
             return True
 
-    def arithmeticExpression(self,a:float,b:float,c:float)->bool:
+    def arithmeticExpression(self, a: float, b: float, c: float) -> bool:
 
-        if a+b==c or a-b==c or a*b==c or a/b==c:
+        if a + b == c or a - b == c or a * b == c or a / b == c:
             return True
         else:
             return False
 
-    def tenisSet(self,score1, score2):#does not work hiden 17/20 test passed
+    def tenisSet(self, score1, score2):  # does not work hiden 17/20 test passed
 
-        if (score1==6 and score2<5) or (score2==6 and score1<5):
+        if (score1 == 6 and score2 < 5) or (score2 == 6 and score1 < 5):
             return True
-        elif ((score1==7  and score1-score2<=2) or (score1==5 and score2-score1<=2)) and (score1!=score2):
+        elif ((score1 == 7 and score1 - score2 <= 2) or (score1 == 5 and score2 - score1 <= 2)) and (score1 != score2):
             return True
         else:
             return False
@@ -108,35 +107,44 @@ class CornerOf0sAnd1s:
 
 class LoopTunnel:
     def __init__(self):
-        self.miLeastFactorial=self.leastFactorial(n=17)
+        self.miLeastFactorial = self.leastFactorial(n=17)
 
-    def leastFactorial(self,n):
+    def leastFactorial(self, n):
         pass
 
-    def countSumOfTwoRepresentations2(self,n, l, r):
+    def countSumOfTwoRepresentations2(self, n, l, r):
         pass
 
-    def magicalWell(self,a, b, n):
+    def magicalWell(self, a, b, n):
         pass
 
-    def lineUp(self,commands):
+    def lineUp(self, commands):
         pass
 
-    def additionWithoutCarrying(self,param1, param2):
+    def additionWithoutCarrying(self, param1, param2):
         pass
-
-
-
-
-
-
-
-
 
 
 class ListForestEdge:
     def __init__(self):
-        pass
+        self.mlArray=self.createArray(size=5)
+        self.mlReplaicement=self.arrayReplace(inputArray=[1,2,1],elemToReplace=1,substitutionElem=3)
+        self.mlFirstReverse=self.firstReverseTry(arr=[1, 2, 3, 4, 5])
+
+    def createArray(self,size):
+            return [1] * size
+
+    def arrayReplace(self,inputArray, elemToReplace, substitutionElem):
+        for i in range(len(inputArray)):
+            if inputArray[i]==elemToReplace:
+                inputArray[i]=substitutionElem
+        return inputArray
+
+    def firstReverseTry(self,arr):
+        f=arr[0]
+        l=arr[-1]
+
+
 
 
 class LabyrinthOfNestedLoops:
